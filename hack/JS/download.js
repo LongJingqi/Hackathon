108fb25fcb4c8cac9a37f7d4f7c5acaec9b5fca9
biao1097501200 <1097501200@qq.com>
2017-11-04 17:39:50 +0800

    download2.0

--- hack/JS/download.js
+++ hack/JS/download.js
@@ -1,35 +1,36 @@
-function start() {
-
-};
 var authorization = '84ddf124-4d18-4f34-a55c-a89b3e836251';
-var formData = new FormData();
-formData.append('file', $('#upload')[0].files[0]);
-var num = 0;
+function start() {
+    var formData = new FormData();
+    formData.append('file', $('#upload')[0].files[0]);
+    var num = 0;
 
-$.ajax({
-    async: false,
-    contentType: 'application/json',
-    url: 'https://openapi.saicmotor.com/services/cloud/sds/v1.0.0/upload?fileName=test%2Ftest.html&applicationName=test',
-    beforeSend: function (request) {
-        request.setRequestHeader('Authorization', 'Bearer ' + authorization);
-    },
-    data: formData,
-    success: function (data) {
-        console.log(data);
-    }
-});
-setInterval(function () {
-    $.ajax({
+    /*$.ajax({
         async: false,
+        type: 'post',
         contentType: 'application/json',
-        url: 'https://openapi.saicmotor.com/services/cloud/sds/v1.0.0/download?fileName=test%2Ftest.html&applicationName=test',
+        url: 'https://openapi.saicmotor.com/services/cloud/sds/v1.0.0/upload?fileName=test%2Ftest.html&applicationName=test',
         beforeSend: function (request) {
             request.setRequestHeader('Authorization', 'Bearer ' + authorization);
         },
+        data: formData,
         success: function (data) {
-            alert(++num);
             console.log(data);
         }
-    });
-}, 5000);
+    });*/
+    setInterval(function () {
+        $.ajax({
+            async: false,
+            contentType: 'application/json',
+            type: 'get',
+            url: 'https://openapi.saicmotor.com/services/cloud/sds/v1.0.0/download?fileName=test%2Ftest.html&applicationName=test',
+            beforeSend: function (request) {
+                request.setRequestHeader('Authorization', 'Bearer ' + authorization);
+            },
+            success: function (data) {
+                console.log(++num);
+                console.log(data);
+            }
+        });
+    }, 2000);
+};
 
